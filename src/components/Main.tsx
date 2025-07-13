6184ee5de1caca291ba1cf3cdb0ba6c8a651237d
VS3301 <varun.s3301@gmail.com>
2025-07-12 21:59:36 -0600

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,15 +13,15 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            //<a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/varun-srinivasan-136316243/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Varun Srinivasan</h1>
           <p>Electrical Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            //<a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/varun-srinivasan-136316243/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
